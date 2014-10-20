90c37259e8197422638edec0bc46b7003e8e115b
Karan Pahawa <kpahawa@gmail.com>
2014-10-20 17:40:11 -0400

    first commit

---
+++ app/controllers/registrants_controller.rb
@@ -0,0 +1,28 @@
+require "pry"
+class RegistrantsController < ApplicationController
+  def index
+     @registrants = Registrant.all
+  end
+
+  def create
+    # binding.pry
+    @registrant = Registrant.new(registrant_params)
+    if @registrant.save
+        redirect_to Registrant, alert: "User created successfully."
+    end
+  end
+  
+  def registrant_params
+    params.require(:registrant).permit(:name, :email)
+  end
+
+  def new
+    @registrant = Registrant.new
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
---
+++ test/helpers/registrant_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class RegistrantHelperTest < ActionView::TestCase
+end
